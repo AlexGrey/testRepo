f01f12a2130fe6a6942e8f237d1dc4f74613b02d
ComradeGrey <ilyareznik91@gmail.com>
2016-01-01 18:39:23 +0400

    test232

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-var http = require('http');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-var server = http.createServer(function(req, res) {
-    res.writeHead(200);
-    res.end('Hello Http');
+io.on('connection', function(socket){
+    socket.on('reg', function(msg){
+        console.log(msg);
+    });
 });
 
-server.listen(8080);
+http.listen(8080, function(){
+    console.log('listening on *:8080');
+});
